57419f9b885f42291f67ffabbdaee3973b154a56
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-24 11:18:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 DIR=$(pwd)
-pkg install zsh -y
+
+# Install paket wajib
+pkg install zsh neofetch proot-distro -y
+
+# Setup Shortcut & Extra Keys
+mkdir -p ~/.termux
+cp $DIR/termux.properties ~/.termux/termux.properties
+termux-reload-settings
+
+# Setup .zshrc
 rm -f ~/.zshrc
-echo "source $DIR/mytheme.zsh-theme" > ~/.zshrc
+echo "neofetch" > ~/.zshrc
+echo "source $DIR/mytheme.zsh-theme" >> ~/.zshrc
+
+# Ganti Shell
 chsh -s zsh
+echo "Selesai! Restart Termux lo."
