502354483dee8e586a896fed46a2beec510f0219
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-24 10:12:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-ZSH_CUSTOM_THEMES="$HOME/.oh-my-zsh/themes"
+echo "Installing Danxy Custom Theme..."
 
-echo "Checking environment..."
+DIR=$(pwd)
 
-if [ ! -d "$ZSH_CUSTOM_THEMES" ]; then
-    echo "Creating themes directory..."
-    mkdir -p "$ZSH_CUSTOM_THEMES"
+pkg install zsh -y
+
+cat <<EOF > ~/.zshrc
+source $DIR/mytheme.zsh-theme
+
+if [ -t 1 ]; then
+  exec zsh
 fi
+EOF
 
-if [ ! -f "$HOME/.zshrc" ]; then
-    echo "Creating .zshrc file..."
-    touch "$HOME/.zshrc"
-fi
+chsh -s zsh
 
-echo "Installing My Custom Zsh Theme..."
-cp mytheme.zsh-theme "$ZSH_CUSTOM_THEMES/mytheme.zsh-theme"
-
-if grep -q "ZSH_THEME=" "$HOME/.zshrc"; then
-    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="mytheme"/' "$HOME/.zshrc"
-else
-    echo 'ZSH_THEME="mytheme"' >> "$HOME/.zshrc"
-fi
-
-echo "Selesai! Silakan ketik: source ~/.zshrc"
+echo "Berhasil! Tema kamu sudah terpasang secara mandiri."
+echo "Silakan buka ulang Termux."
