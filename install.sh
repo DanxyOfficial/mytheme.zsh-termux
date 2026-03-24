09af342f7819cf1a1f518c55561f70d3d27b8fe1
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-24 10:04:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+ZSH_CUSTOM_THEMES="$HOME/.oh-my-zsh/themes"
+
+echo "Checking environment..."
+
+if [ ! -d "$ZSH_CUSTOM_THEMES" ]; then
+    echo "Creating themes directory..."
+    mkdir -p "$ZSH_CUSTOM_THEMES"
+fi
+
+if [ ! -f "$HOME/.zshrc" ]; then
+    echo "Creating .zshrc file..."
+    touch "$HOME/.zshrc"
+fi
+
 echo "Installing My Custom Zsh Theme..."
+cp mytheme.zsh-theme "$ZSH_CUSTOM_THEMES/mytheme.zsh-theme"
 
-# Salin file tema ke folder Oh My Zsh (jika user pakai OMZ)
-cp mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
+if grep -q "ZSH_THEME=" "$HOME/.zshrc"; then
+    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="mytheme"/' "$HOME/.zshrc"
+else
+    echo 'ZSH_THEME="mytheme"' >> "$HOME/.zshrc"
+fi
 
-# Ubah settingan di .zshrc secara otomatis
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="mytheme"/' ~/.zshrc
-
-echo "Selesai! Silakan ketik 'source ~/.zshrc' atau buka ulang Termux."
+echo "Selesai! Silakan ketik: source ~/.zshrc"
