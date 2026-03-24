0c81a83b9d25e8746e0058465743f5e5debbf5af
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-24 12:16:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR=$(pwd)
-pkg install zsh proot-distro -y
+pkg install zsh proot-distro git -y
+mkdir -p ~/.zsh
+[[ ! -d ~/.zsh/zsh-autosuggestions ]] && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+[[ ! -d ~/.zsh/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 mkdir -p ~/.termux
 if [ -f "$DIR/termux.properties" ]; then
     cp "$DIR/termux.properties" ~/.termux/termux.properties
@@ -11,7 +14,3 @@ touch $PREFIX/etc/motd
 rm -f ~/.zshrc
 echo "source $DIR/mytheme.zsh-theme" > ~/.zshrc
 chsh -s zsh
-echo "-----------------------------------------------"
-echo " Style Termux selesai di perbarui silahkan:"
-echo " restart Termux atau ketik: zsh"
-echo "-----------------------------------------------"
