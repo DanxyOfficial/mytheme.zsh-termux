9b9b8a97b4c1da178fe27d96cee9420ad14fbc63
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-24 10:28:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
-echo "Installing Danxy Custom Theme..."
-
 DIR=$(pwd)
-
 pkg install zsh -y
-
-cat <<EOF > ~/.zshrc
-source $DIR/mytheme.zsh-theme
-
-if [ -t 1 ]; then
-  exec zsh
-fi
-EOF
-
+rm -f ~/.zshrc
+echo "source $DIR/mytheme.zsh-theme" > ~/.zshrc
 chsh -s zsh
-
-echo "Berhasil! Tema kamu sudah terpasang secara mandiri."
-echo "Silakan buka ulang Termux."
