11399f6b3a49584d537055a554fd4e3070f9e128
Danxy Official ☕︎ <dpras19800@gmail.com>
2026-03-26 01:01:45 +0700

    Update boot_banner.sh

--- boot_banner.sh
+++ boot_banner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Functions untuk gambar kotak
 PUT(){ echo -en "\033[${1};${2}H";}
 DRAW(){ echo -en "\033%";echo -en "\033(0";}
 WRITE(){ echo -en "\033(B";}
 
-tput civis
+tput civis # Sembunyiin kursor pas gambar
 clear
-tput setaf 2
+tput setaf 2 # Warna hijau
 DRAW
 echo "lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk"
 echo "x                                                           x"
@@ -16,10 +17,10 @@ echo "x                                                           x"
 echo "x                                                           x"
 echo "x                                                           x"
 echo "x                                                           x"
-mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj"
+echo "mqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj"
 WRITE
 
-# Tampilkan Tulisan t3rmux (pastiin pkg figlet & lolcat terinstall)
+# Tampilkan Tulisan t3rmux (pake figlet & lolcat)
 PUT 4 5
 if command -v figlet &> /dev/null && command -v lolcat &> /dev/null; then
     figlet -f shadow " t3rmux" | lolcat
@@ -28,10 +29,10 @@ else
     echo -e "\e[1;32mt3rmux\e[0m"
 fi
 
-# Info Versi
+# Info Versi di pojok bawah kotak
 PUT 9 40
 echo -e "\e[35mBoot Script \e[33m2.0\e[0m"
 
-# Balikin kursor ke bawah kotak
+# Balikin posisi kursor ke bawah kotak
 PUT 11 0
-tput cnorm
+tput cnorm # Tampilin kursor lagi
